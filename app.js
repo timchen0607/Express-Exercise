1bee8f92412d1ea13f8e789a50b3f944cf9f1843
Tim School <a0963573232@gmail.com>
2022-01-10 15:52:07 +0800

    Feat: 基本路由

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-let express = require("express");
-let app = express();
+let app = require("express")();
+
+// 基本路由
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/a", (req, res) => {
+  res.send("Hello A");
+});
+
+// 監聽 port
+app.listen(5000);
