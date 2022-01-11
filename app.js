30e7790e7e4eea9762be0aa43fb9f6faaeb03b2c
Tim Home <a0963573232@gmail.com>
2022-01-11 12:26:23 +0800

    Feats: query 實作

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 let app = require("express")();
 
-// 基本路由
+// 基本路由，http://localhost:5000/
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-// params
-app.get("/params/:uid", (req, res) => {
+// params，http://localhost:5000/p/5
+app.get("/p/:uid", (req, res) => {
   res.send("Hello " + req.params.uid);
+});
+// query，http://localhost:5000/q?limit=21
+app.get("/q/", (req, res) => {
+  let limit = req.query.limit;
+  res.send("Hello" + limit);
 });
 
 // 監聽 port，如無預設 port 則 port 為 5000
